209c75196a06d7c7888c650f4f2e1845e060f140
Lukasz Malinowski <s19743@pjwstk.edu.pl>
2021-03-10 21:15:33 +0100

    add reading html from http

--- Crawler/Program.cs
+++ Crawler/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,12 @@ namespace Crawler
     {
         static void Main(string[] args)
         {
+            HttpClient client = new HttpClient();
+            HttpResponseMessage message = client.GetAsync("https://blog.hubspot.com/marketing/professional-email-address").Result;
+
+            string htmlString = message.Content.ReadAsStringAsync().Result;
+
+            Console.WriteLine(htmlString);
         }
     }
 }
