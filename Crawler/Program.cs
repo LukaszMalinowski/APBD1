165206a3b8e5c4b6e1536dea8a385ef3a29f579a
Lukasz Malinowski <s19743@pjwstk.edu.pl>
2021-03-12 15:52:48 +0100

    complete zadanie4

--- Crawler/Program.cs
+++ Crawler/Program.cs
@@ -2,7 +2,7 @@
 using System.Net.Http;
 using System.Collections;
 using System.Text.RegularExpressions;
-
+using System.Collections.Generic;
 
 namespace Crawler
 {
@@ -10,35 +10,82 @@ namespace Crawler
     {
         static void Main(string[] args)
         {
+            if (args.Length == 0)
+                throw new ArgumentNullException("Pass url in program argument");
+
+            if (ArgumentInvalid(args[0]))
+                throw new ArgumentException("Url not validated!");
+
             HttpClient client = new HttpClient();
-            HttpResponseMessage message = client.GetAsync(args[0]).Result;
+            string htmlString = null;
 
-            string htmlString = message.Content.ReadAsStringAsync().Result;
-
-            string[] emails = GetEmailsFromHtml(htmlString);
-
-            foreach (string email in emails)
+            try
             {
-                Console.WriteLine(email);
-            }
+                HttpResponseMessage message = client.GetAsync(args[0]).Result;
 
+                htmlString = message.Content.ReadAsStringAsync().Result;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Błąd w czasue pobierania strony");
+            }
+            finally
+            {
+                client.Dispose();
+            }
+                        
+            try
+            {
+                string[] emails = GetEmailsFromHtml(htmlString);
+
+                foreach (string email in emails)
+                {
+                    Console.WriteLine(email);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
             Console.ReadKey();
+        }
+
+        static bool ArgumentInvalid(string url)
+        {
+            //regex validating url adress
+            string pattern = @"(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})";
+
+            Regex regex = new Regex(pattern);
+
+            if (regex.IsMatch(url))
+                return false;
+
+            return true;
         }
 
         static string[] GetEmailsFromHtml(string html)
         {
+            //regex that finds email adress
             string pattern = @"\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*";
 
             Regex regex = new Regex(pattern);
 
-            ArrayList emails = new ArrayList();
+            HashSet<string> emails = new HashSet<string>();
 
             foreach (Match match in regex.Matches(html))
             {
                 emails.Add(match.Value);
             }
 
-            return (string[]) emails.ToArray( typeof(string));
+            if (emails.Count < 1)
+                throw new Exception("Nie znaleziono adresów email");
+
+            string[] returnArray = new string[emails.Count];
+
+            emails.CopyTo(returnArray);
+
+            return returnArray;
         }
     }
 }
